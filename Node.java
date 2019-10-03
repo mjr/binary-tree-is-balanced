d3ce87b6fe12a019f3b9b1f31d1668c5128dbf80
Manaia Junior <manaiajr.23@gmail.com>
2019-10-03 12:40:39 -0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,57 @@
+public class Node {
+    private Node left;
+    private Node right;
+    private int value;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void insert(Node node) {
+        if (node.value < this.value) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.insert(node);
+            }
+        } else if (node.value > this.value) {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.insert(node);
+            }
+        }
+    }
+
+    public Node search(int key) {
+        if (key == this.value) {
+            return this;
+        }
+
+        if (key < this.value) {
+            if (this.left != null) {
+                return this.left.search(key);
+            }
+        }
+
+        if (key > this.value) {
+            if (this.right != null) {
+                return this.right.search(key);
+            }
+        }
+
+        return null;
+    }
+}
